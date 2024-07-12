049a68cd0dbf4c84fc6a5987232f0fc1ad1bf6f4
Popachka <swarm21s@mail.ru>
2024-07-12 21:59:14 +0300

    added front

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,14 +8,16 @@ class Settings(BaseSettings):
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
+    print(os.path.dirname(os.path.abspath(__file__)))
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env"),
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "../.env"),
         extra='ignore'
     )
+    print(model_config)
 
 
 settings = Settings()
-
+print(settings)
 
 def get_db_url():
     return (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
