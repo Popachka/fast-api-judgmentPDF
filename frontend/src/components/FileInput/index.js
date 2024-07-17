687f523ffa7567d537bbb57aa4ae40283c3bfc16
Popachka <swarm21s@mail.ru>
2024-07-17 20:02:28 +0300

    d

--- frontend/src/components/FileInput/index.js
+++ frontend/src/components/FileInput/index.js
@@ -22,7 +22,7 @@ const FileInput = ({ setUploadedFiles }) => {
     });
 
     try {
-      const response = await fetch("http://localhost:8000/api/upload-pdf/", {
+      const response = await fetch("https://judgmentfiles.online/api/upload-pdf", {
         method: "POST",
         body: formData,
       });
